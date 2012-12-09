12b158a8585c880d004854df916218e32675b4b3
hongy <hongy@hongy-xubuntu.(none)>
2012-12-10 02:52:30 +0800

    toc

---
+++ src/toc/toc.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+#include "toc.h"
+
+int add(int a, int b) {
+	return a+b;
+}
+
+void print(char * s, int n) {
+	char ss[256];
+	strncpy(ss, s, 256);
+	printf("%s[%d]\n", ss, n);
+}
